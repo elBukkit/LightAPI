a8721d5fa835d3ca21ade266d9b2d67cda2e5f6d
Nathan Wolf <nathan@elmakers.com>
2018-02-20 15:26:05 -0800

    Add a stripped down API-only Maven-ized version

---
+++ src/main/java/ru/beykerykt/lightapi/LightAPI.java
@@ -0,0 +1,74 @@
+/**
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2015 - 2016
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package ru.beykerykt.lightapi;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LightAPI extends JavaPlugin implements Listener {
+
+	private static LightAPI plugin;
+
+	@SuppressWarnings("static-access")
+	@Override
+	public void onLoad() {
+		this.plugin = this;
+	}
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	public void log(CommandSender sender, String message) {
+		sender.sendMessage(ChatColor.AQUA + "<LightAPI>: " + ChatColor.WHITE + message);
+	}
+
+	public static LightAPI getInstance() {
+		return plugin;
+	}
+
+	public static boolean createLight(Location location, int lightlevel, boolean async) {
+		return createLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), lightlevel, async);
+	}
+
+	public static boolean createLight(final World world, final int x, final int y, final int z, final int lightlevel, boolean async) {
+		return false;
+	}
+
+	public static boolean deleteLight(Location location, boolean async) {
+		return deleteLight(location.getWorld(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), async);
+	}
+
+	public static boolean deleteLight(final World world, final int x, final int y, final int z, boolean async) {
+		return false;
+	}
+}
